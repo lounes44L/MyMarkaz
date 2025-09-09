4ad07d3dd7322b4194318183c5e7abf5a75ee1d4
llalaoui <lalaouilounes2@gmail.com>
2025-09-09 09:02:16 +0200

    Fix path concatenation in settings.py for Render deployment

--- gestion_markaz/settings.py
+++ gestion_markaz/settings.py
@@ -132,11 +132,11 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
