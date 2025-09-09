ca683ee42b4dae7251595fa1c4848ec78f73504b
llalaoui <lalaouilounes2@gmail.com>
2025-09-09 09:43:49 +0200

    Migration vers PostgreSQL: Mise à jour de la configuration et des dépendances

--- gestion_markaz/settings.py
+++ gestion_markaz/settings.py
@@ -82,11 +82,13 @@ WSGI_APPLICATION = 'gestion_markaz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
+# Configuration de base de données PostgreSQL
+DATABASE_URL = 'postgresql://mymarkaz_db_user:0h93846YIkE2dvJXW2mdhzcQUObhWYhN@dpg-d2vt40ili9vc738cpveg-a.virginia-postgres.render.com/mymarkaz_db'
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(DATABASE_URL)
 }
 
 
